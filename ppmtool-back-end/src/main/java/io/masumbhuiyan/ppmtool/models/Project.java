90fba1b8d8991e948eaaa2ba686beecbe1baa31f
MasumBhuiyan <masumbhuiyan577@gmail.com>
2020-09-21 21:30:59 +0600

    added Project object validation

--- ppmtool-back-end/src/main/java/io/masumbhuiyan/ppmtool/models/Project.java
+++ ppmtool-back-end/src/main/java/io/masumbhuiyan/ppmtool/models/Project.java
@@ -1,11 +1,14 @@
 package io.masumbhuiyan.ppmtool.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -17,12 +20,24 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Project name is required.")
     private String projectName;
+
+    @NotBlank(message = "Project Identifier is required.")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters.")
     private String projectIdentifier;
+
+    @NotBlank(message = "Project description is required.")
     private String projectDescription;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     @PrePersist
