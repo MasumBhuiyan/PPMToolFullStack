23749c6f03bd4856e7ca0abda35a59f50c8d9ae1
MasumBhuiyan <masumbhuiyan577@gmail.com>
2020-09-22 16:15:30 +0600

    project service  created

--- ppmtool-back-end/src/main/java/io/masumbhuiyan/ppmtool/services/ProjectService.java
+++ ppmtool-back-end/src/main/java/io/masumbhuiyan/ppmtool/services/ProjectService.java
@@ -1,5 +1,6 @@
 package io.masumbhuiyan.ppmtool.services;
 
+import io.masumbhuiyan.ppmtool.execptions.ProjectIdentifierException;
 import io.masumbhuiyan.ppmtool.models.Project;
 import io.masumbhuiyan.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,31 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project save(Project project) {
-        return projectRepository.save(project);
+    public Project createOrUpdateProject(Project project) {
+        try {
+            return projectRepository.save(project);
+        } catch (Exception exception) {
+            throw new ProjectIdentifierException("Project identifier " + project.getProjectIdentifier() + " already exists.");
+        }
+    }
+
+    public Project findProjectByIdentifier(String projectIdentifier) {
+        Project project = projectRepository.findByProjectIdentifier(projectIdentifier);
+        if( project == null ) {
+            throw new ProjectIdentifierException("Project identifier " + projectIdentifier + " does not exists.");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectIdentifier) {
+        Project project = projectRepository.findByProjectIdentifier(projectIdentifier);
+        if (project == null) {
+            throw new ProjectIdentifierException("Can not delete project with identifier " + projectIdentifier + ", because it does not exist.");
+        }
+        projectRepository.delete(project);
     }
 }
